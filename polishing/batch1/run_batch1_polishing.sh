85baf3dc9656c1a7b0887fd148e02c7b318be088
Mira <mastorasmira@gmail.com>
2023-12-19 20:32:20 -0800

    add launch polishing

--- polishing/batch1/run_batch1_polishing.sh
+++ polishing/batch1/run_batch1_polishing.sh
@@ -19,21 +19,33 @@ python3 ../../../hpc/launch_from_table.py \
 ###############################################################################
 
 ## on HPC...
-cd /private/groups/hprc/
+cd /private/groups/hprc/polishing
+
+## clone repo
+
+git clone https://github.com/human-pangenomics/hprc_intermediate_assembly.git
 
 ## check that github repo is up to date
 git -C /private/groups/hprc/hprc_intermediate_assembly pull
 
-mkdir assembly/batch1
-cd assembly/batch1
+## get wdl workflow from github
+git clone https://github.com/miramastoras/hpp_production_workflows.git
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hpp_production_workflows/ pull
+
+# manually add in polishing docker v0.0.8_12122023 until its publicly released
+vim /private/groups/hprc/polishing/hpp_production_workflows/QC/wdl/workflows/hprc_DeepPolisher.wdl
+
+mkdir batch1
+cd batch1
 
 ## get files to run hifiasm in sandbox...
-cp -r /private/groups/hprc/hprc_intermediate_assembly/assembly/batch1/* ./
+cp -r /private/groups/hprc/polishing/hprc_intermediate_assembly/polishing/batch1/* ./
 
-
-mkdir hifiasm_submit_logs
+mkdir hprc_DeepPolisher_submit_logs
 
 ## launch with slurm array job
 sbatch \
-     launch_hifiasm_array.sh \
-     intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3.csv
+     launch_hprc_deepPolisher_batch1_array.sh \
+     intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira.csv
