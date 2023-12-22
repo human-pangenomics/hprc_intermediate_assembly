6a3eb5791bf15aac31c0888362c6c0e937afebaa
Mira <mastorasmira@gmail.com>
2023-12-22 11:08:43 -0800

    relaunch

--- polishing/batch1/launch_hprc_deepPolisher_batch1.sh
+++ polishing/batch1/launch_hprc_deepPolisher_batch1.sh
@@ -46,7 +46,7 @@ SINGULARITY_CACHEDIR=`pwd`/outputs/cache/.singularity/cache
 MINIWDL__SINGULARITY__IMAGE_CACHE=`pwd`/outputs/cache/.cache/miniwdl
 export TOIL_SLURM_ARGS="--time=2-0:00 --partition=high_priority"
 
-toil-wdl-runner \
+time toil-wdl-runner \
     --jobStore ./polishing_bigstore \
     --batchSystem slurm \
     --batchLogsDir ./toil_logs \
