7eefd7a211ddc3124ec1c3677ad4227894c70fdb
Mira <mastorasmira@gmail.com>
2024-01-02 14:32:48 -0800

    relaunch HG02258

--- polishing/batch2/run_batch2_polishing.sh
+++ polishing/batch2/run_batch2_polishing.sh
@@ -47,6 +47,12 @@ sbatch \
      launch_hprc_deepPolisher_batch2.sh \
      intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2.csv
 
+# resubmit HG02258 (#7)
+
+sbatch \
+     launch_hprc_deepPolisher_batch2_relaunch.sh \
+     intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2.csv
+
 ###############################################################################
 ##                             write output files to csv                     ##
 ###############################################################################
@@ -59,3 +65,8 @@ python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_tab
       --output_data_table ./intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2.updated.csv \
       --json_location '{sample_id}_hprc_DeepPolisher_outputs.json' \
       --submit_logs_directory hprc_DeepPolisher_submit_logs
+
+python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table ./intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2.csv \
+      --output_data_table ./intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2.updated.csv \
+      --json_location '{sample_id}_hprc_DeepPolisher_outputs.json'
