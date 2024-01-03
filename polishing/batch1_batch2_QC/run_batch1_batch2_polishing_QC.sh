58189fb9931479941cce4249c9bdba397f122cc1
Mira <mastorasmira@gmail.com>
2024-01-03 14:15:53 -0800

    relaunch with hg38 as reference for extracting cram

--- polishing/batch1_batch2_QC/run_batch1_batch2_polishing_QC.sh
+++ polishing/batch1_batch2_QC/run_batch1_batch2_polishing_QC.sh
@@ -25,3 +25,25 @@ python3 ../../../hpc/launch_from_table.py \
 ###############################################################################
 ##                             create launch polishing                      ##
 ###############################################################################
+
+## on HPC...
+cd /private/groups/hprc/polishing
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hprc_intermediate_assembly pull
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hpp_production_workflows/ pull
+
+mkdir batch1_batch2_QC
+cd batch1_batch2_QC
+
+## get files to run hifiasm in sandbox...
+cp -r /private/groups/hprc/polishing/hprc_intermediate_assembly/polishing/batch1_batch2_QC/* ./
+
+mkdir hprc_polishing_QC_submit_logs
+
+## launch with slurm array job
+sbatch \
+     launch_batch1_batch2_polishingQC.sh \
+     intAsm_batch1_batch2_polishingQC_sample_table.csv
