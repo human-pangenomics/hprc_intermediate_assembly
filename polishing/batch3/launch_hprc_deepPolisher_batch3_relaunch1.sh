0b39fe152576a9bc9bc8d0640b4b560f0a509e27
Mira <mastorasmira@gmail.com>
2024-01-14 12:23:39 -0700

    restart

--- polishing/batch3/launch_hprc_deepPolisher_batch3_relaunch1.sh
+++ polishing/batch3/launch_hprc_deepPolisher_batch3_relaunch1.sh
@@ -58,6 +58,7 @@ time toil-wdl-runner \
     --runLocalJobsOnWorkers \
     --retryCount 1 \
     --disableProgress \
+    --restart \
     --clusterStats ${sample_id}_clusterstats.json \
     2>&1 | tee log.txt
 
