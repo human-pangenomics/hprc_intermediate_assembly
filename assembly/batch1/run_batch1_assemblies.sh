5c947f8efebf8a638a2aec9737e76d8da6eaa918
juklucas <juklucas@ucsc.edu>
2024-01-19 16:48:44 -0800

    update notes

--- assembly/batch1/run_batch1_assemblies.sh
+++ assembly/batch1/run_batch1_assemblies.sh
@@ -15,7 +15,7 @@ python3 ../../../hpc/launch_from_table.py \
 ## add/commit/push to github (hprc_intermediate_assembly)
 
 ###############################################################################
-##                             create launch assemblies                      ##   
+##                                 launch assemblies                         ##   
 ###############################################################################
 
 ## on HPC...
@@ -34,6 +34,18 @@ cp -r /private/groups/hprc/hprc_intermediate_assembly/assembly/batch1/* ./
 mkdir hifiasm_submit_logs
 
 ## launch with slurm array job
+## failed, relaunch after pushing fixes
 sbatch \
      launch_hifiasm_array.sh \
      intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3.csv
+
+## relaunch
+sbatch \
+     launch_hifiasm_array_retry.sh \
+     intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3.csv     
+
+## rerun two samples that failed...
+sbatch \
+     launch_hifiasm_array_relaunch.sh \
+     intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3.csv 
+
