d807fa53cb0dc7e93b5727d31ba36853c24972c4
juklucas <juklucas@ucsc.edu>
2024-02-08 16:59:39 -0800

    initial commit batch 4

---
+++ assembly/batch4/run_batch4_assemblies.sh
@@ -0,0 +1,40 @@
+
+###############################################################################
+##                             create input jsons                            ##
+###############################################################################
+
+## on personal computer...
+
+cd /Users/juklucas/Desktop/github/hprc_intermediate_assembly/assembly/batch4/hifiasm_input_jsons
+
+python3 ../../../hpc/launch_from_table.py \
+     --data_table ../HPRC_Intermediate_Assembly_s3Locs_batch4.csv \
+     --field_mapping ../hifiasm_hic_input_mapping.csv \
+     --workflow_name hifiasm
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
+###############################################################################
+##                                 launch assemblies                         ##
+###############################################################################
+
+## on HPC...
+cd /private/groups/hprc/
+
+## check that github repo is up to date
+git -C /private/groups/hprc/hprc_intermediate_assembly pull
+
+mkdir assembly/batch4
+cd assembly/batch4
+
+## get files to run hifiasm in sandbox...
+cp -r /private/groups/hprc/hprc_intermediate_assembly/assembly/batch4/* ./
+
+
+mkdir hifiasm_submit_logs
+
+## launch with slurm array job
+sbatch --array=[1-24]%24 \
+     launch_hifiasm_array.sh \
+     HPRC_Intermediate_Assembly_s3Locs_batch4.csv
+     
