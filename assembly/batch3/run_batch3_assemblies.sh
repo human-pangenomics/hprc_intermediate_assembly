1459b32603f911a28ed8e9d2175df2e3ff83639b
juklucas <juklucas@ucsc.edu>
2024-02-09 10:25:36 -0800

    extract QC

--- assembly/batch3/run_batch3_assemblies.sh
+++ assembly/batch3/run_batch3_assemblies.sh
@@ -80,3 +80,81 @@ awk '$2 ~ /ERROR/ { print $1 }' hifiasm_status.txt \
 sbatch \
      launch_hifiasm_array_rerun.sh \
      HPRC_Intermediate_Assembly_s3Locs_Batch3_rerun.csv
+
+
+###############################################################################
+##                         Update table with outputs                         ##
+###############################################################################
+
+cd /private/groups/hprc/assembly/batch3
+
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table HPRC_Intermediate_Assembly_s3Locs_Batch3.csv \
+      --output_data_table HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm.csv \
+      --json_location '{sample_id}_hifiasm_outputs.json'
+
+
+###############################################################################
+##                        create input jsons for initial QC                  ##   
+###############################################################################
+
+mkdir initial_qc
+cd initial_qc
+
+cp ../../batch2/initial_qc/qc_input_mapping.csv ./
+
+mkdir qc_input_jsons
+cd qc_input_jsons
+
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ../../HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm.csv \
+     --field_mapping ../qc_input_mapping.csv \
+     --workflow_name initial_qc    
+
+## copy to github repo for notetaking
+cp \
+    HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm.csv \
+    /private/groups/hprc/hprc_intermediate_assembly/assembly/batch3/
+
+cp -r initial_qc/ /private/groups/hprc/hprc_intermediate_assembly/assembly/batch3/
+
+
+###############################################################################
+##                               launch initial QC                           ##   
+###############################################################################
+
+cd /private/groups/hprc/assembly/batch3
+
+mkdir qc_submit_logs
+
+sbatch \
+    --array=[1-17]%8 \
+    initial_qc/launch_initial_qc_array.sh \
+    HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm.csv 
+
+
+###############################################################################
+##                     Update table with hifiasm qc outputs                  ##
+###############################################################################
+
+# on hpc after entire batch has finished
+cd /private/groups/hprc/assembly/batch3
+
+## collect location of QC results
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm.csv \
+      --output_data_table HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm_w_qc.csv \
+      --json_location '{sample_id}_hifiasm_qc_outputs.json'
+
+## extract QC results
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/misc/extract_initial_qc.py \
+     --qc_data_table HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm_w_qc.csv \
+     --extract_column_name filtQCStats \
+     --output initial_qc/batch3_extracted_qc_results.csv
+
+# cp \
+#      intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_hifiasm.csv \
+#      /private/groups/hprc/hprc_intermediate_assembly/assembly/batch1/intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_hifiasm.csv
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
