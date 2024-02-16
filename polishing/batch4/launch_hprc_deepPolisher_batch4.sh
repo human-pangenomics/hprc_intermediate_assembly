3cd80593c4b724c516519829bdfdf07ab6f1f29f
Mira <mastorasmira@gmail.com>
2024-02-15 17:45:11 -0800

    launch batch4

---
+++ polishing/batch4/launch_hprc_deepPolisher_batch4.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Author      : Julian Lucas, juklucas@ucsc.edu
+# Description : Launch toil job submission for HPRC's DeepPolisher workflow on a single machine
+# Usage       : sbatch launch_hifiasm_array.sh sample_file.csv
+#               	sample_file.csv should have a header (otherwised first sample will be skipped)
+#					and the sample names should be in the first column
+
+#SBATCH --job-name=HPRC-DeepPolisher-batch4
+#SBATCH --cpus-per-task=32
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=high_priority
+#SBATCH --mail-user=mmastora@ucsc.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mem=200gb
+#SBATCH --threads-per-core=1
+#SBATCH --output=hprc_DeepPolisher_submit_logs/hprcDeepPolisher_submit_%x_%j_%A_%a.log
+#SBATCH --time=7-0:00
+#SBATCH --array=1-17%17
+
+set -ex
+
+## Pull samples names from CSV passed to script
+sample_file=$1
+
+# Read the CSV file and extract the sample ID for the current job array task
+# Skip first row to avoid the header
+sample_id=$(awk -F ',' -v task_id=${SLURM_ARRAY_TASK_ID} 'NR>1 && NR==task_id+1 {print $1}' "${sample_file}")
+
+# Ensure a sample ID is obtained
+if [ -z "${sample_id}" ]; then
+    echo "Error: Failed to retrieve a valid sample ID. Exiting."
+    exit 1
+fi
+
+echo "${sample_id}"
+
+## Create then change into sample directory...
+mkdir -p ${sample_id}
+cd ${sample_id}
+
+# make folder on local node for s3 data
+LOCAL_FOLDER=/data/tmp/$(whoami)/HPRC_DeepPolisher_${sample_id}
+mkdir -p ${LOCAL_FOLDER}
+
+mkdir -p toil_logs
+mkdir -p ${LOCAL_FOLDER}/hprc_DeepPolisher_outputs
+
+# create new json
+cp ../hprc_DeepPolisher_input_jsons/${sample_id}_hprc_DeepPolisher.json ${LOCAL_FOLDER}/${sample_id}_hprc_DeepPolisher.json
+
+# loop through s3 links, download them to LOCAL_FOLDER,
+# then replace them in the new json file
+grep s3 ../hprc_DeepPolisher_input_jsons/${sample_id}_hprc_DeepPolisher.json \
+| sed 's|,||g' | sed 's|["'\'']||g' | while read line ; do
+    FILENAME=`basename $line`
+    if [[ ! -e ${LOCAL_FOLDER}/${FILENAME} ]] ; then
+        aws s3 cp --no-sign-request $line ${LOCAL_FOLDER}/
+    fi
+    sed -i "s|${line}|${LOCAL_FOLDER}/${FILENAME}|g" ${LOCAL_FOLDER}/${sample_id}_hprc_DeepPolisher.json
+done
+
+export SINGULARITY_CACHEDIR=`pwd`/../cache/.singularity/cache
+export MINIWDL__SINGULARITY__IMAGE_CACHE=`pwd`/../cache/.cache/miniwdl
+export TOIL_SLURM_ARGS="--time=7-0:00 --partition=high_priority"
+export TOIL_COORDINATION_DIR=/data/tmp
+
+toil clean "${LOCAL_FOLDER}/jobstore"
+
+set -o pipefail
+set +e
+time toil-wdl-runner \
+    --jobStore "${LOCAL_FOLDER}/jobstore" \
+    --stats \
+    --clean=never \
+    --batchSystem single_machine \
+    --maxCores "${SLURM_CPUS_PER_TASK}" \
+    --batchLogsDir ./toil_logs \
+    /private/groups/hprc/polishing/hpp_production_workflows/QC/wdl/workflows/hprc_DeepPolisher.wdl \
+    ${LOCAL_FOLDER}/${sample_id}_hprc_DeepPolisher.json \
+    --outputDirectory ${LOCAL_FOLDER}/hprc_DeepPolisher_outputs \
+    --outputFile ${sample_id}_hprc_DeepPolisher_outputs.json \
+    --runLocalJobsOnWorkers \
+    --retryCount 1 \
+    --disableProgress \
+    2>&1 | tee log.txt
+EXITCODE=$?
+set -e
+
+toil stats --outputFile stats.txt "${LOCAL_FOLDER}/jobstore"
+
+if [[ "${EXITCODE}" == "0" ]] ; then
+    echo "Succeeded."
+
+    # copy polished fasta, polishing vcf to /private/groups/hprc
+    mkdir -p hprc_DeepPolisher_outputs
+    cp ${LOCAL_FOLDER}/hprc_DeepPolisher_outputs/*.fasta hprc_DeepPolisher_outputs/
+    cp ${LOCAL_FOLDER}/hprc_DeepPolisher_outputs/polisher_output.vcf.gz hprc_DeepPolisher_outputs/
+    cp ${LOCAL_FOLDER}/hprc_DeepPolisher_outputs/*PHARAOH* hprc_DeepPolisher_outputs/
+
+    # Clean up
+    rm -Rf ${LOCAL_FOLDER}
+else
+    echo "Failed."
+    exit "${EXITCODE}"
+fi
---
+++ polishing/batch4/run_batch4_polishing.sh
@@ -0,0 +1,76 @@
+###############################################################################
+##                             create input jsons                            ##
+###############################################################################
+
+## on personal computer...
+
+# Remove top up data from data table
+
+cd /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/polishing/batch4/hprc_DeepPolisher_input_jsons
+
+python3 ../../../hpc/launch_from_table.py \
+     --data_table ../HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm_w_qc.csv \
+     --field_mapping ../hprc_DeepPolisher_input_mapping.csv \
+     --workflow_name hprc_DeepPolisher
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
+###############################################################################
+##                             create launch polishing                      ##
+###############################################################################
+
+## on HPC...
+cd /private/groups/hprc/polishing
+
+## clone repo
+
+git clone https://github.com/human-pangenomics/hprc_intermediate_assembly.git
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hprc_intermediate_assembly pull
+
+## get wdl workflow from github
+git clone https://github.com/miramastoras/hpp_production_workflows.git
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hpp_production_workflows/ pull
+
+mkdir batch4
+cd batch4
+
+## get files to run hifiasm in sandbox...
+cp -r /private/groups/hprc/polishing/hprc_intermediate_assembly/polishing/batch4/* ./
+
+mkdir hprc_DeepPolisher_submit_logs
+
+## Launch batch 4
+
+sbatch \
+     launch_hprc_deepPolisher_batch4.sh \
+     HPRC_Intermediate_Assembly_s3Locs_Batch3_w_hifiasm_w_qc.csv
+
+###############################################################################
+##                             write output files to csv                     ##
+###############################################################################
+
+# update output file jsons
+cd /private/groups/hprc/polishing/batch3
+
+grep -v "sample_id" HPRC_Intermediate_Assembly_s3Locs_Batch2.updated.noTopUp.csv | cut -f1 -d "," \
+| while read line ; do sample_id=$line ; cp ${sample_id}/${sample_id}_hprc_DeepPolisher_outputs.json ${sample_id}/${sample_id}_hprc_DeepPolisher_outputs_updated.json; done
+
+grep -v "sample_id" HPRC_Intermediate_Assembly_s3Locs_Batch2.updated.noTopUp.csv | cut -f1 -d "," \
+| while read line ; do sample_id=$line ; \
+sed 's|,|\n|g' ${sample_id}/${sample_id}_hprc_DeepPolisher_outputs.json | \
+cut -f 2 -d ":" | sed 's| ||g' | sed 's|}||g' | sed 's|"||g' | while read line ; do file=`basename $line`;\
+newpath="/private/groups/hprc/polishing/batch3/${sample_id}/hprc_DeepPolisher_outputs/${file}" ; \
+sed -i "s|${line}|${newpath}|g" ${sample_id}/${sample_id}_hprc_DeepPolisher_outputs_updated.json ;done; done
+
+# on hprc after entire batch has finished
+cd /private/groups/hprc/polishing/batch3
+
+python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table ./HPRC_Intermediate_Assembly_s3Locs_Batch2.updated.noTopUp.csv \
+      --output_data_table ./HPRC_Intermediate_Assembly_s3Locs_Batch2.updated.noTopUp.updated.csv \
+      --json_location '{sample_id}_hprc_DeepPolisher_outputs_updated.json' \
+      --submit_logs_directory hprc_DeepPolisher_submit_logs
