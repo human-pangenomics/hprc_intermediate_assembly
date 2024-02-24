b2a9792e8d64b629eb6906d92030cea475a7b6dc
Mira <mastorasmira@gmail.com>
2024-02-23 17:04:06 -0800

    launch k31 QC

---
+++ polishing/batch2/apply_GQ_filter/hprc_polishing_QC_k31/run_hprc_polishing_QC_batch2.sh
@@ -0,0 +1,50 @@
+###############################################################################
+##                             create input jsons                            ##
+###############################################################################
+
+## on personal computer...
+mkdir -p /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/polishing/batch2/apply_GQ_filter/hprc_polishing_QC_k31/hprc_polishing_QC_input_jsons
+cd /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/polishing/batch2/apply_GQ_filter/hprc_polishing_QC_k31/hprc_polishing_QC_input_jsons
+
+python3 /Users/miramastoras/Desktop/Paten_lab/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ../intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2_noTopUp_updated.filterVcf.polished.csv \
+     --field_mapping ../hprc_polishing_QC_input_mapping.csv \
+     --workflow_name hprc_polishing_QC
+
+## add/commit/push to github (hprc_intermediate_assembly)
+
+###############################################################################
+##                             create launch polishing                      ##
+###############################################################################
+
+## on HPC...
+mkdir -p /private/groups/hprc/polishing/batch2/apply_GQ_filter/hprc_polishing_QC_k31/
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hprc_intermediate_assembly pull
+
+## check that github repo is up to date
+git -C /private/groups/hprc/polishing/hpp_production_workflows/ pull
+
+## get files to run hifiasm in sandbox...
+cp -r /private/groups/hprc/polishing/hprc_intermediate_assembly/polishing/batch2/apply_GQ_filter/hprc_polishing_QC_k31/* ./
+
+mkdir hprc_polishing_QC_submit_logs
+
+## launch with slurm array job
+sbatch \
+     launch_hprc_polishing_QC_batch2.sh \
+     intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2_noTopUp_updated.filterVcf.polished.csv
+
+###############################################################################
+##                             write output files to csv                     ##
+###############################################################################
+
+# on hprc after entire batch has finished
+cd /private/groups/hprc/polishing/batch2/apply_GQ_filter/hprc_polishing_QC_k21
+
+python3 /private/groups/hprc/polishing/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table ./intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2_noTopUp.csv \
+      --output_data_table ./intermAssembl_batch1_sample_table_20231204_WUSTLonly_s3_mira_polishing_batch2_noTopUp.updated.csv \
+      --json_location '{sample_id}_hprc_polishing_QC_outputs.json' \
+      --submit_logs_directory hprc_polishing_QC_submit_logs
