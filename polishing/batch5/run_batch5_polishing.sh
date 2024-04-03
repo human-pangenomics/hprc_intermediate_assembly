2a32da5afd7c4d97502684e5c089815640d8aeee
Mira <mastorasmira@gmail.com>
2024-04-03 15:23:37 -0700

    add partition

--- polishing/batch5/run_batch5_polishing.sh
+++ polishing/batch5/run_batch5_polishing.sh
@@ -34,8 +34,6 @@ cd batch5
 ## get files to run hifiasm in sandbox...
 cp -r /private/groups/hprc/polishing/hprc_intermediate_assembly/polishing/batch5/* ./
 
-mkdir hprc_DeepPolisher_submit_logs
-
 mkdir slurm_logs
 export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 
@@ -43,12 +41,13 @@ export PYTHONPATH="/private/home/juklucas/miniconda3/envs/toil/bin/python"
 sbatch \
      --job-name=hprc-DeepPolisher-batch5 \
      --array=[1-24]%24 \
+     --partition=high_priority \
      --cpus-per-task=32 \
      --mem=400gb \
      /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
      --wdl /private/groups/hprc/polishing/hpp_production_workflows/QC/wdl/workflows/hprc_DeepPolisher.wdl \
      --sample_csv HPRC_Intermediate_Assembly_s3Locs_Batch4_w_hifiasm_w_QC.csv \
-     --input_json_path './hprc_DeepPolisher_input_jsons/${SAMPLE_ID}_hprc_DeepPolisher.json'
+     --input_json_path '../hprc_DeepPolisher_input_jsons/${SAMPLE_ID}_hprc_DeepPolisher.json'
 
 ###############################################################################
 ##                             write output files to csv                     ##
