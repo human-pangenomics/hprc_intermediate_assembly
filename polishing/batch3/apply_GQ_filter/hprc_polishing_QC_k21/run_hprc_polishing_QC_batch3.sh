1d76b3c3d035290a2bfeef059d5dba14995affa0
Mira <mastorasmira@gmail.com>
2024-04-24 15:06:07 -0700

    relaunch failed samples with new yak docker

--- polishing/batch3/apply_GQ_filter/hprc_polishing_QC_k21/run_hprc_polishing_QC_batch3.sh
+++ polishing/batch3/apply_GQ_filter/hprc_polishing_QC_k21/run_hprc_polishing_QC_batch3.sh
@@ -37,6 +37,19 @@ sbatch \
      launch_hprc_polishing_QC_batch3.sh \
      HPRC_Intermediate_Assembly_s3Locs_Batch2.updated.noTopUp.updated.filterVcf.polished.csv
 
+# relaunch samples which failed due to yak error
+sbatch \
+     --job-name=hprc-polishing_QC_k21-batch3 \
+     --array=[29,32,3,5,4,8,10]%8 \
+     --partition=high_priority \
+     --cpus-per-task=32 \
+     --mem=400gb \
+     --mail-type=FAIL,END \
+     --mail-user=mmastora@ucsc.edu \
+     /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
+     --wdl /private/groups/hprc/polishing/hpp_production_workflows/QC/wdl/workflows/hprc_polishing_QC.wdl \
+     --sample_csv HPRC_Intermediate_Assembly_s3Locs_Batch2.updated.noTopUp.updated.filterVcf.polished.csv \
+     --input_json_path '../hprc_polishing_QC_input_jsons/${SAMPLE_ID}_hprc_polishing_QC.json'
 ###############################################################################
 ##                             write output files to csv                     ##
 ###############################################################################
