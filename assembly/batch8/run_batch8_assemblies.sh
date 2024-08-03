6ac1e16ea7de3469b2f5dda1a7be8badf4166cf2
apblair <apblair@ucsc.edu>
2024-08-02 17:13:08 -0700

    update batch8

--- assembly/batch8/run_batch8_assemblies.sh
+++ assembly/batch8/run_batch8_assemblies.sh
@@ -1,4 +1,3 @@
-
 ###############################################################################
 ##                             create input jsons                            ##
 ###############################################################################
@@ -6,11 +5,10 @@
 ## on HPC...
 cd /private/groups/hprc/assembly
 
-mkdir batch8
-cd batch8
-
 ## check that github repo is up to date
-git -C /private/groups/hprc/hprc_intermediate_assembly pull
+# git -C /private/groups/hprc/hprc_intermediate_assembly pull
+git clone git@github.com:human-pangenomics/hpp_production_workflows.git
+git clone git@github.com:human-pangenomics/hprc_intermediate_assembly.git
 
 ## get sample sheet
 cp /private/groups/hprc/hprc_intermediate_assembly/assembly/batch8/HPRC_Assembly_s3Locs_Batch8.csv ./
@@ -19,13 +17,22 @@ mkdir hifiasm_input_jsons
 cd hifiasm_input_jsons
 
 ## get input mapping; make sure to include q-score cutoff!
-cp /private/groups/hprc/hprc_intermediate_assembly/assembly/batch5/hifiasm_input_jsons/hifiasm_hic_input_mapping.csv ./
-
+# create hifiasm_hic_input_mapping.csv
 
 python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/launch_from_table.py \
      --data_table ../HPRC_Assembly_s3Locs_Batch8.csv \
      --field_mapping hifiasm_hic_input_mapping.csv \
      --workflow_name hic_hifiasm_assembly_cutadapt_multistep
+
+# ```rmarkdown
+# Review parameters with hifiasm_hic_input_mapping using womtools.
+
+# java -jar /private/groups/hprc/human-pangenomics/cromwell/womtool-54.jar \
+#   inputs \
+# 	/private/groups/hprc/assembly/batch8/hpp_production_workflows/assembly/wdl/workflows/hic_hifiasm_assembly_cutadapt_multistep.wdl  \
+#   | grep -v "optional"
+
+# ```
 
 
 ###############################################################################
@@ -35,8 +42,8 @@ python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/launch_from_table.py
 cd /private/groups/hprc/assembly/batch8
 
 ## check again that github repo is up to date
-git -C /private/groups/hprc/hprc_intermediate_assembly pull
-git -C /private/home/juklucas/github/hpp_production_workflows/ pull
+git -C hprc_intermediate_assembly pull
+git -C hpp_production_workflows pull
 
 mkdir slurm_logs
 
@@ -46,11 +53,10 @@ sbatch \
      --cpus-per-task=64 \
      --mem=430gb \
      --partition=high_priority \
-     /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
-     --wdl /private/home/juklucas/github/hpp_production_workflows/assembly/wdl/workflows/hic_hifiasm_assembly_cutadapt_multistep.wdl \
-     --sample_csv HPRC_Assembly_s3Locs_Batch8.csv \
-     --input_json_path '../hifiasm_input_jsons/${SAMPLE_ID}_hic_hifiasm_assembly_cutadapt_multistep.json' 
-
+     /private/groups/hprc/assembly/batch8/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
+     --wdl /private/groups/hprc/assembly/batch8/hpp_production_workflows/assembly/wdl/workflows/hic_hifiasm_assembly_cutadapt_multistep.wdl \
+     --sample_csv /private/groups/hprc/assembly/batch8/HPRC_Assembly_s3Locs_Batch8.csv \
+     --input_json_path '/private/groups/hprc/assembly/batch8/hifiasm_input_jsons/${SAMPLE_ID}_hic_hifiasm_assembly_cutadapt_multistep.json'
 
 ###############################################################################
 ##                         Update table with outputs                         ##
@@ -63,6 +69,49 @@ python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/update_table_with_ou
       --output_data_table HPRC_Assembly_s3Locs_batch8_w_hifiasm.csv \
       --json_location '{sample_id}_hic_hifiasm_assembly_cutadapt_multistep_outputs.json'
 
+###############################################################################
+##                           Create QC Input JSONs                           ##   
+###############################################################################
+
+cd /private/groups/hprc/assembly/batch8
+mkdir -p initial_qc
+cd initial_qc
+
+
+mkdir qc_input_jsons
+cd qc_input_jsons
+
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ../../HPRC_Assembly_s3Locs_batch8_w_hifiasm.csv \
+     --field_mapping ../hic_qc_input_mapping.csv \
+     --workflow_name initial_qc    
+
+
+###############################################################################
+##                               launch initial QC                           ##   
+###############################################################################
+
+cd /private/groups/hprc/assembly/batch8
+
+# mkdir qc_submit_logs
+
+sbatch \
+     --job-name=HPRC-qc-batch8 \
+     --array=[1-30]%30 \
+     --partition=high_priority \
+     /private/groups/hprc/assembly/batch8/hprc_intermediate_assembly/hpc/toil_sbatch_slurm.sh \
+     --wdl /private/groups/hprc/assembly/batch8/hpp_production_workflows/QC/wdl/workflows/comparison_qc.wdl \
+     --sample_csv HPRC_Assembly_s3Locs_batch8_w_hifiasm.csv \
+     --input_json_path '/private/groups/hprc/assembly/batch8/initial_qc/qc_input_jsons/${SAMPLE_ID}_initial_qc.json'
+
+# ```rmarkdown
+# Review comparison_qc.wdl parameters 
+
+# java -jar /private/groups/hprc/human-pangenomics/cromwell/womtool-54.jar \
+#   inputs \
+# 	/private/groups/hprc/assembly/batch8/hpp_production_workflows/QC/wdl/workflows/comparison_qc.wdl  \
+#   | grep -v "optional"
+# ```
 
 ###############################################################################
 ##                           Create QC Input JSONs                           ##   
@@ -73,16 +122,17 @@ cd /private/groups/hprc/assembly/batch8
 mkdir -p initial_qc
 cd initial_qc
 
-cp /private/groups/hprc/assembly/batch4/initial_qc/qc_input_mapping.csv ./
+# NOTE: this depends on trio or hic
+cp /private/groups/hprc/assembly/batch7/initial_qc/qc_input_mapping.csv ./
 
 mkdir qc_input_jsons
 cd qc_input_jsons
+
 
 python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/launch_from_table.py \
      --data_table ../../HPRC_Assembly_s3Locs_batch8_w_hifiasm.csv \
      --field_mapping ../qc_input_mapping.csv \
      --workflow_name initial_qc    
-
 
 ###############################################################################
 ##                               launch initial QC                           ##   
@@ -94,7 +144,7 @@ mkdir qc_submit_logs
 
 sbatch \
      --job-name=HPRC-qc-batch8 \
-     --array=[1-14]%14 \
+     --array=[1-30]%30 \
      --partition=high_priority \
      /private/groups/hprc/hprc_intermediate_assembly/hpc/toil_sbatch_slurm.sh \
      --wdl /private/home/juklucas/github/hpp_production_workflows/QC/wdl/workflows/comparison_qc.wdl \
@@ -108,7 +158,7 @@ sbatch \
 cd /private/groups/hprc/assembly/batch8
 
 ## collect location of QC results
-python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+python3 /private/groups/hprc/assembly/batch8/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
       --input_data_table HPRC_Assembly_s3Locs_batch8_w_hifiasm.csv  \
       --output_data_table HPRC_Assembly_s3Locs_batch8_w_hifiasm_w_QC.csv  \
       --json_location '{sample_id}_comparison_qc_outputs.json'
@@ -118,19 +168,3 @@ python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/misc/extract_initial
      --qc_data_table HPRC_Assembly_s3Locs_batch8_w_hifiasm_w_QC.csv \
      --extract_column_name filtQCStats \
      --output initial_qc/batch8_extracted_qc_results.csv
-
-# ## copy to github repo for notetaking
-# cp HPRC_Assembly_s3Locs_batch8_w_*.csv \
-#      /private/groups/hprc/hprc_intermediate_assembly/assembly/batch8/
-
-# cp hifiasm_input_jsons/* \
-#      /private/groups/hprc/hprc_intermediate_assembly/assembly/batch8/hifiasm_input_jsons/
-
-
-# mv blah.csv initial_qc/t2t_counts.csv
-
-# ## git add, commit, push
-# cp -r \
-#      initial_qc/ \
-#      /private/groups/hprc/hprc_intermediate_assembly/assembly/batch8/
-
