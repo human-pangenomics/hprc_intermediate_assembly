3cc445e375105f9d7aeabe156ccc74ec951320c4
mobinasri <masri@ucsc.edu>
2025-01-12 11:17:43 -0800

    Added scripts and tables for output s3 links

---
+++ assembly_qc/batch1/hmm_flagger/ont/create_output_ont_csv.sh
@@ -0,0 +1,30 @@
+cd /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/ont
+
+touch csv2tab && chmod u+x csv2tab
+
+#Add to it
+cat <<EOF > csv2tab
+#!/usr/bin/env python
+import csv, sys
+csv.writer(sys.stdout, dialect='excel-tab').writerows(csv.reader(sys.stdin))
+EOF
+
+OUTPUT_TSV="hmm_flagger_ont_data_table.output_s3.csv"
+BASE="https://s3-us-west-2.amazonaws.com/human-pangenomics/submissions/ca366a13-5bad-487b-8a57-97344e9aa0e4--HPRC_RELEASE_2_SUPPLEMENTARY_ASSEMBLY_QC"
+printf "sample_id,mapping_info,bam_link,bai_link,cov_gz_link,hmm_flagger_v1.1_stats_tsv_link,hmm_flagger_v1.1_nohap_bed_link,hmm_flagger_v1.1_prediction_bed_link\n" > ${OUTPUT_TSV}
+
+
+while read line; do
+	SAMPLE=$(echo $line | ./csv2tab | awk -v RS='\r\n'  -F'\t' '{print $1}')
+	MAPPING_SUFFIX=$(echo $line | ./csv2tab | awk -v RS='\r\n'  -F'\t' '{print $23}')
+	if [[ ${SAMPLE} == "sample_id" ]];then
+		continue
+	fi
+	BAM=${BASE}/${SAMPLE}/hprc_r2/assembly_qc/read_alignments/ont/${SAMPLE}.${MAPPING_SUFFIX}.corrected.bam
+	BAI=${BASE}/${SAMPLE}/hprc_r2/assembly_qc/read_alignments/ont/${SAMPLE}.${MAPPING_SUFFIX}.corrected.bam.bai
+	COV_GZ=${BASE}/${SAMPLE}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/${SAMPLE}.${MAPPING_SUFFIX}.corrected.downsample_1.0.augmented.cov.gz
+	TSV_v1dot1=${BASE}/${SAMPLE}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/${SAMPLE}.${MAPPING_SUFFIX}.corrected.downsample_1.0.augmented.stats.tsv
+	BED_NOHAP_v1dot1=${BASE}/${SAMPLE}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/${SAMPLE}.hmm_flagger_v1.1.0.hmm_flagger.no_Hap.bed
+	BED_PRED_v1dot1=${BASE}/${SAMPLE}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/${SAMPLE}.${MAPPING_SUFFIX}.corrected.downsample_1.0.hmm_flagger_prediction.bed
+	printf "${SAMPLE},${MAPPING_SUFFIX},${BAM},${BAI},${COV_GZ},${TSV_v1dot1},${BED_NOHAP_v1dot1},${BED_PRED_v1dot1}\n"
+done < hmm_flagger_ont_data_table.csv >> ${OUTPUT_TSV}
--- assembly_qc/batch1/hmm_flagger/run_hmm_flagger.sh
+++ assembly_qc/batch1/hmm_flagger/run_hmm_flagger.sh
@@ -115,6 +115,7 @@ sbatch      --job-name=${WDL_NAME}_${USERNAME} \
             --mail-user=${EMAIL} \
             --mail-type=FAIL,END \
             --output=${WDL_NAME}_logs/${WDL_NAME}_%A_%a.log \
+	    --exclude=phoenix-[23] \
             --array=[1-141]%30  \
             --time=${TIME_LIMIT} \
             --partition=${PARTITION} \
@@ -214,3 +215,193 @@ python3 /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/hpc/updat
       --input_data_table ${WORKING_DIR}/hmm_flagger_hifi_data_table.csv  \
       --output_data_table ${WORKING_DIR}/hmm_flagger_hifi_data_table.output.csv  \
       --json_location '{sample_id}_hmm_flagger_end_to_end_with_mapping_outputs.json'
+
+
+###############################################################################
+##                 Upload hifi output files to s3 bucket                     ##
+###############################################################################
+
+
+cd /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/hifi
+
+rm -rf s3_upload
+mkdir -p s3_upload
+cd s3_upload
+
+cat <<EOF > hmm_flagger_hifi_upload_linking_map.csv
+column_name,destination
+coverageGz,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+biasTableTsv,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+finalPredictionBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+intermediatePredictionBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+loglikelihoodTsv,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+miscFlaggerFilesTarGz,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+fullStatsTsv,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+projectionSexBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+projectionSDBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+projectionAnnotationsBedArray,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+bigwigArray,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_hifi/
+readAlignmentBam,upload/{sample_id}/hprc_r2/assembly_qc/read_alignments/hifi/
+readAlignmentBai,upload/{sample_id}/hprc_r2/assembly_qc/read_alignments/hifi/
+EOF
+
+awk -F',' 'NR==1 {print $1",sample_hap,"substr($0, index($0,$2))} NR>1 {split($1, a, "_hap"); print a[1]","$1","substr($0, index($0,$2))}' \
+    ../hmm_flagger_hifi_data_table.output.csv \
+    > hmm_flagger_hifi_data_table.outputs_sample_oriented.csv
+
+
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/misc/link_to_subfolder.py \
+     --csv_file hmm_flagger_hifi_data_table.outputs_sample_oriented.csv \
+     --mapping_csv hmm_flagger_hifi_upload_linking_map.csv
+
+# run a bash script for upload to s3
+sbatch ../../upload_hifi_data.sh 
+
+
+
+###############################################################################
+##                 Create hifi output table with s3 links                     ##
+###############################################################################
+
+
+cd /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/hifi
+bash create_output_hifi_csv.sh
+
+
+###########################################
+##        Create input jsons   (ONT)     ##
+###########################################
+
+# set working directory
+WORKING_DIR="/private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/ont"
+cd ${WORKING_DIR}
+
+# check that flagger repo is up to date
+FLAGGER_DIR="/private/groups/patenlab/masri/apps/flagger_v1.1.0/flagger"
+git -C ${FLAGGER_DIR} pull
+
+## Save WDL path and name in environment variables
+WDL_PATH=${FLAGGER_DIR}/wdls/workflows/hmm_flagger_end_to_end_with_mapping.wdl
+WDL_FILENAME=$(basename ${WDL_PATH})
+WDL_NAME=${WDL_FILENAME%%.wdl}
+
+
+## Make a folder for saving files related to run e.g. input and output jsons
+cd ${WORKING_DIR}
+mkdir -p runs_toil_slurm
+cd runs_toil_slurm
+
+## Make a directory for saving input json files
+mkdir -p ${WDL_NAME}_input_jsons
+cd ${WDL_NAME}_input_jsons
+
+python3 /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/hpc/launch_from_table.py \
+     --data_table ${WORKING_DIR}/hmm_flagger_ont_data_table.csv \
+     --field_mapping ${WORKING_DIR}/hmm_flagger_ont_input_mapping.csv \
+     --workflow_name ${WDL_NAME}
+
+
+
+#########################################################################################
+##                             Launch Mapping + HMM-Flagger     (ONT)                 ##
+#########################################################################################
+
+
+## Make sure you are in the working directory
+cd ${WORKING_DIR}
+
+## Set environment variables for sbatch
+USERNAME="masri"
+EMAIL="masri@ucsc.edu"
+TIME_LIMIT="70:00:00"
+
+## Partition should be modifed based on the available partitions on the server
+PARTITION="high_priority"
+
+
+## Go to the execution directory
+mkdir -p runs_toil_slurm/${WDL_NAME}_logs
+cd runs_toil_slurm
+
+## Run jobs arrays
+sbatch      --job-name=${WDL_NAME}_${USERNAME} \
+            --cpus-per-task=64 \
+            --mem=256G \
+            --mail-user=${EMAIL} \
+            --mail-type=FAIL,END \
+            --output=${WDL_NAME}_logs/${WDL_NAME}_%A_%a.log \
+            --exclude=phoenix-[23] \
+            --array=[1-10]%10  \
+            --time=${TIME_LIMIT} \
+            --partition=${PARTITION} \
+            /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/hpc/toil_sbatch_single_machine.sh \
+            --wdl ${WDL_PATH} \
+            --sample_csv  ${WORKING_DIR}/hmm_flagger_ont_data_table.csv \
+            --input_json_path ${WORKING_DIR}/runs_toil_slurm/${WDL_NAME}_input_jsons/\${SAMPLE_ID}_${WDL_NAME}.json
+
+
+
+###############################################################################
+##                             write output files to csv                     ##
+###############################################################################
+
+# set working directory
+WORKING_DIR="/private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/ont"
+cd ${WORKING_DIR}/runs_toil_slurm
+
+## collect location of QC results
+python3 /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/hpc/update_table_with_outputs.py \
+      --input_data_table ${WORKING_DIR}/hmm_flagger_ont_data_table.csv  \
+      --output_data_table ${WORKING_DIR}/hmm_flagger_ont_data_table.output.csv  \
+      --json_location '{sample_id}_hmm_flagger_end_to_end_with_mapping_outputs.json'
+
+###############################################################################
+##                 Upload ont output files to s3 bucket                     ##
+###############################################################################
+
+
+cd /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/ont
+
+rm -rf s3_upload
+mkdir -p s3_upload
+cd s3_upload
+
+cat <<EOF > hmm_flagger_ont_upload_linking_map.csv
+column_name,destination
+coverageGz,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+biasTableTsv,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+finalPredictionBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+intermediatePredictionBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+loglikelihoodTsv,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+miscFlaggerFilesTarGz,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+fullStatsTsv,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+projectionSexBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+projectionSDBed,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+projectionAnnotationsBedArray,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+bigwigArray,upload/{sample_id}/hprc_r2/assembly_qc/hmm_flagger/v1.1.0_ont/
+readAlignmentBam,upload/{sample_id}/hprc_r2/assembly_qc/read_alignments/ont/
+readAlignmentBai,upload/{sample_id}/hprc_r2/assembly_qc/read_alignments/ont/
+EOF
+
+awk -F',' 'NR==1 {print $1",sample_hap,"substr($0, index($0,$2))} NR>1 {split($1, a, "_hap"); print a[1]","$1","substr($0, index($0,$2))}' \
+    ../hmm_flagger_ont_data_table.output.csv \
+    > hmm_flagger_ont_data_table.outputs_sample_oriented.csv
+
+
+python3 /private/groups/hprc/hprc_intermediate_assembly/hpc/misc/link_to_subfolder.py \
+     --csv_file hmm_flagger_ont_data_table.outputs_sample_oriented.csv \
+     --mapping_csv hmm_flagger_ont_upload_linking_map.csv
+
+# run a bash script for upload to s3
+sbatch ../../upload_ont_data.sh
+
+
+###############################################################################
+##                 Create ont output table with s3 links                     ##
+###############################################################################
+
+
+cd /private/groups/hprc/qc_hmm_flagger/hprc_intermediate_assembly/assembly_qc/batch1/hmm_flagger/ont
+bash create_output_ont_csv.sh
+
+
